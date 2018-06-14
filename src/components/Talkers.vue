73920b0bc89cb7df7267eaad27050a8a0f30ed9f
DianQK <dianqk@icloud.com>
2018-06-14 13:51:09 +0800

    增加 Croath 和 Anddy

--- src/components/Talkers.vue
+++ src/components/Talkers.vue
@@ -15,7 +15,7 @@
       <div class="introduction">
         {{ talker.introduction }}
       </div>
-      <img class="picture" :srcset="`${talker.picture} 2x`" sizes="(max-width: 800px) 20px, (max-width: 800px) 20px" />
+      <img v-if="talker.picture" class="picture" :srcset="`${talker.picture} 2x`" sizes="(max-width: 800px) 20px, (max-width: 800px) 20px" />
     </div>
   </div>
 </template>
@@ -27,9 +27,9 @@ export default {
     return {
       talkers: [
         {
-          names: [''],
+          names: ['刘镇夫'],
           links: [{ title: 'WEBSITE' }, { title: 'TWITTER' }],
-          introduction: `Type Sam is a strange mashup of developer, writer and trainer. By day you'll find him recording videos for Razeware, writing tutorials, attending conferences and generally being a good guy. By night he's likely to be out entertaining people, armed with his trombone and killer dance moves.`,
+          introduction: `小鱼(Croath)，刘镇夫，Flight School 飞行学校译者，NSHipster 作者和中文社区译者。全栈工程师，主要方向在移动研发 Swift 方向，活跃于开源社区。目前在创业公司，主要负责加密货币技术的移动整合工作。`,
           picture: require('../assets/talks/xiaoyu.png'),
           style: `color: white; background: url(${require('../assets/talks/talk_background_element.png')}) bottom right no-repeat / 525px 448px, #243338;`
         },
@@ -53,7 +53,14 @@ export default {
           introduction: `王巍现在是一名专注于 iOS 的职业开发者。他拥有对于 Swift，Objective-C 以及网络程序设计方面的深厚知识及多年实践经验。他将这些技术运用于创建应用程序和通用框架等解决方案，其中很多都被用户喜爱，并被开发者社区广泛使用来构建其他软件。同时，他是 ObjC 中国组织的发起人和领导者，为将最佳 iOS 实践和进阶技术引入中国而不断努力。`,
           picture: require('../assets/talks/onevcat.png'),
           style: `color: #4C4C4C; background: url(${require('../assets/talks/talk_background_element.png')}) bottom right no-repeat / 525px 448px, #BAA9B3;`
-        }
+        },
+        {
+          names: ['Anddy'],
+          links: [{ title: 'WEBSITE' }, { title: 'TWITTER' }],
+          introduction: `Andyy is a Software Engineer with a long history of eight years working within the iOS SDK. Over these years, he's seen first hand how the platform has grown and changed whilst the developers find new ways to deliver new features and functionality to users whilst working within the confines of the walled garden.
+Apart from being a Software Engineer, he's also the organiser of Playgrounds Conference, founder of Wu-Tang Clang which is a learn-to-code fundraiser, keeps a technical blog of Swift on Medium, and was a co-organiser for his local CocoaHeads Melbourne monthly iOS meetup.`,
+          style: `color: white; background: url(${require('../assets/talks/talk_background_element.png')}) bottom right no-repeat / 525px 448px, #243338;`
+        },
       ]
     }
   }
